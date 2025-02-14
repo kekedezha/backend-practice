0dbe7dc4cb6960827ad26b210653a56c36464abc
kekedezha <dezhac6@gmail.com>
2025-02-14 09:57:41 -0500

    imported and enabled cors and add port # to .env file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+// initialize environment variables from config file
 import "dotenv/config";
+// import cors module from cors package
+import cors from "cors";
+// import express module from express package
+import express from "express";
 
-console.log("Hello Node.js project.");
+// create an instance of an Express application
+// This object will provide methods to define routes,
+// handle middleware and configure the server
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/example", (req, res) => {
+  res.send("This will be the example page");
+});
+
+// have the app start listening for incoming HTTP requests
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
