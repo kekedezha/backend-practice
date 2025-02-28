0753ce1453e2938b23687b03745ea705520089da
kekedezha <dezhac6@gmail.com>
2025-02-28 15:19:15 -0500

    added in delete funcs to delete users and messages

--- rw.js
+++ rw.js
@@ -4,7 +4,6 @@ import fs from "fs";
 export const readData = () => {
   try {
     const data = fs.readFileSync("./data.json", "utf8");
-    console.log(data);
     return JSON.parse(data);
   } catch (error) {
     console.error("Error reading data:", error);
@@ -21,5 +20,43 @@ export const appendNewMessage = (newData) => {
 export const appendNewUser = (newData) => {
   const data = readData();
   data.users[newData.id] = newData;
+  fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+};
+
+export const deleteUser = (userId) => {
+  // Create empty object that will hold data after deleted user
+  const after_deletion_JSON = {};
+  // Read in data from data.json file
+  const data = readData();
+
+  // filter through object and delete user with specified id
+  const after_deletion = Object.values(data.users).filter(
+    (user) => user.id != userId
+  );
+  // transfer data stored in array to object
+  after_deletion.forEach((item) => (after_deletion_JSON[item.id] = item));
+
+  // overwrite users value to json object read in the beginning
+  data.users = after_deletion_JSON;
+
+  fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+};
+
+export const deleteMessage = (messageId) => {
+  // Create empty object that will hold data after deleted message
+  const after_deletion_JSON = {};
+  // Read in data from data.json file
+  const data = readData();
+
+  // filter through object and delete message with specified id
+  const after_deletion = Object.values(data.messages).filter(
+    (message) => message.id != messageId
+  );
+  // transfer data stored in array to object
+  after_deletion.forEach((item) => (after_deletion_JSON[item.id] = item));
+
+  // overwrite messages value to json object read in the beginning
+  data.messages = after_deletion_JSON;
+
   fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
 };
