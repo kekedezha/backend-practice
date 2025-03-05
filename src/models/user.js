f8513cf35424ece6a8f1a7f984e8bf0d05615bac
kekedezha <dezhac6@gmail.com>
2025-03-05 18:36:03 -0500

    added user model file; will be using Sequelize as ORM in conjunction with postgres

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+const getUserModel = (sequelize, { DataTypes }) => {
+  const User = sequelize.define("user", {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  });
+
+  return User;
+};
+
+export default getUserModel;
