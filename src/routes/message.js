95ec204f31f2773a11a78f9e27f77d4c8b956675
kekedezha <dezhac6@gmail.com>
2025-03-05 15:49:04 -0500

    restructuring code and files to make project more modular

---
+++ src/routes/message.js
@@ -0,0 +1,90 @@
+import { Router } from "express";
+// import read/write functions
+import { appendNewMessage, deleteMessage, updateMessage } from "../../rw";
+//import v4 function from uuid package
+import { v4 as uuidv4 } from "uuid";
+
+const router = Router();
+
+// middleware to check if message id exists. For all routes that have the "/messages/:messageId" path
+router.param("messageId", (req, res, next, messageId) => {
+  const message = Object.values(req.context.messages).find(
+    (message) => message.id == messageId
+  );
+
+  if (!message) {
+    return res
+      .status(404)
+      .send("Sorry, no message with thad id exists. Try again.");
+  }
+
+  next();
+});
+
+router.get("/", (req, res) => {
+  return res.send(Object.values(req.context.messages));
+});
+
+router.get("/:messageId", (req, res) => {
+  return res.send(req.context.messages[req.params.messageId]);
+});
+
+router.post("/", (req, res) => {
+  // create unique id for new message
+  const id = uuidv4();
+
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.body.userId,
+  };
+
+  if (message.text.trim() == "" || message.userId.trim() == "") {
+    return res
+      .status(400)
+      .send(
+        "Sorry, text and/or userId missing from body. Missing information. Please try again."
+      );
+  }
+
+  appendNewMessage(message);
+  return res.send(message);
+});
+
+router.put("/:messageId", (req, res) => {
+  const messageId = req.params.messageId;
+  let newText = "";
+  let newUser = "";
+
+  if (req.body.text != undefined) {
+    newText = req.body.text;
+  }
+  if (req.body.userId != undefined) {
+    newUser = req.body.userId;
+  }
+
+  if (newText.trim() == "" && newUser.trim() == "") {
+    return res
+      .status(400)
+      .send(
+        "Sorry, text and userId missing from body. Missing information. Please try again."
+      );
+  } else if (newText.trim() == "") {
+    updateMessage(messageId, null, newUser);
+  } else if (newUser.trim() == "") {
+    updateMessage(messageId, newText, null);
+  }
+
+  return res.send(
+    `PUT HTTP method on message/${req.params.messageId} resource`
+  );
+});
+
+router.delete("/:messageId", (req, res) => {
+  deleteMessage(req.params.messageId);
+  return res.send(
+    `DELETE HTTP method on message/${req.params.messageId} resource`
+  );
+});
+
+export default router;
---
+++ src/routes/user.js
@@ -0,0 +1,80 @@
+import { Router } from "express";
+//import read/write functions
+import { appendNewUser, deleteUser, updateUser } from "../../rw";
+
+const router = Router();
+
+router.param("userId", (req, res, next, userId) => {
+  const user = Object.values(users).find((user) => user.id == userId);
+
+  if (!user) {
+    return res
+      .status(404)
+      .send("Sorry, no user with that id exists. Try again.");
+  }
+
+  next();
+});
+
+// GET routes to retrieve all users messages, each endpoint has its own route
+router.get("/", (req, res) => {
+  return res.send(Object.values(req.context.users));
+});
+
+// GET routes for retrieving a single user or a single message
+router.get("/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+// POST routes to post a new user or a new message
+router.post("/", (req, res) => {
+  const newUserId =
+    parseInt(
+      Object.values(req.context.users)[
+        Object.values(req.context.users).length - 1
+      ].id
+    ) + 1;
+
+  const user = {
+    id: newUserId.toString(),
+    username: req.body.username,
+  };
+
+  if (user.username.trim() == "") {
+    return res
+      .status(400)
+      .send(
+        "Sorry, username missing from body. Missing information. Please try again."
+      );
+  }
+  appendNewUser(user);
+  return res.send(user);
+});
+
+// PUT route to update a user or message
+router.put("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let newUsername;
+
+  if (req.body.username != undefined) {
+    newUsername = req.body.username;
+  }
+
+  if (newUsername.trim() == "") {
+    return res
+      .status(400)
+      .send(
+        "Sorry, username missing from body. Missing information. Please try again."
+      );
+  }
+  updateUser(userId, newUsername);
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+// DELETE route to delete a user or message for the specified id
+router.delete("/:userId", (req, res) => {
+  deleteUser(req.params.userId);
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
+
+export default router;
