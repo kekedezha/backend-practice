bd6f64b799792b28a919f8f0bbdfd5a68bb430fb
kekedezha <dezhac6@gmail.com>
2025-03-06 14:46:44 -0500

    added ./models/index.js file to instantiate sequilize and database models for user and message

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+
+import getMessageModel from "./model";
+import getUserModel from "./user";
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: "postgres",
+  }
+);
+
+const models = {
+  User: getUserModel(sequelize, Sequelize),
+  Message: getMessageModel(sequelize, Sequelize),
+};
+
+Object.keys(models).forEach((key) => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export { sequelize };
+
+export default models;
