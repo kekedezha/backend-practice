7afc911345d3a0d57b204e67773cd84141aa6dc4
kekedezha <dezhac6@gmail.com>
2025-03-06 14:21:14 -0500

    updated user model to have a 1 to N relationship with messages model

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,20 @@
 const getUserModel = (sequelize, { DataTypes }) => {
   const User = sequelize.define("user", {
     username: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
+      type: DataTypes.STRING, //username must be a string
+      unique: true, //username must be unique
+      allowNull: false, //username must not be null
       validate: {
-        notEmpty: true,
+        notEmpty: true, //username must not be empty
       },
     },
   });
+
+  User.associate = (models) => {
+    //User has a 1 to N relationship with Messages model
+    //on user deletion, cascade delete all messages
+    User.hasMany(models.Message, { onDelete: "CASCADE" });
+  };
 
   return User;
 };
