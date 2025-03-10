d184fbbecd67dde5d83d96ad947aec3f3e09bf7d
kekedezha <dezhac6@gmail.com>
2025-03-10 15:20:15 -0400

    addd in data to seed db upon re-init on every Express server restart

--- src/index.js
+++ src/index.js
@@ -43,7 +43,43 @@ app.get("/", (req, res) => {
 
 // have the app start listening for incoming HTTP requests
 sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
   app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`);
   });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "rwieruch",
+      messages: [
+        {
+          text: "Published the Road to learn React",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+
+  await models.User.create(
+    {
+      username: "ddavids",
+      messages: [
+        {
+          text: "Happy to release...",
+        },
+        {
+          text: "Published a complete ...",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+};
